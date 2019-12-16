bac462821324856e811e37133671b8daf717ba42
webmaster0307 <web.master0307@gmail.com>
2019-12-16 22:55:22 +1000

    backend configuration

--- server-animation-blog/config/initializers/session_store.rb
+++ server-animation-blog/config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://client-animation-blog.herokeuapp.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://client-animation-blog.herokeuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
